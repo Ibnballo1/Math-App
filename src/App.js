a210a135394060e56ce1fa452ef23d8551e206dd
Ibnballo1 <belloabdullateef035@gmail.com>
2022-08-05 10:04:15 +0100

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable linebreak-style */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable linebreak-style */
+import React from 'react';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Calculator from './components/Calculator';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 );
 
