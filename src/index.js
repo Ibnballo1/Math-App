cafa998698774bad7f75822339405fa8d847a407
Ibnballo1 <belloabdullateef035@gmail.com>
2022-08-06 17:59:21 +0100

    Fix eslint error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import
+'./index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
