a3e9832ba2fc6ee57d0eb171b0f55e784cab4142
Ibnballo1 <belloabdullateef035@gmail.com>
2022-08-06 17:52:56 +0100

    Add use state to App and also use calculate function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable linebreak-style */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable linebreak-style */
-import React from 'react';
+import React, { useState } from 'react';
 import Calculator from './components/Calculator';
+import calculate from './logic/calculate';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [item, setItem] = useState({
+    calcV: {},
+    res: '0',
+  });
+
+  const handler = (event) => {
+    const { calcV } = item;
+    const calc = calculate(calcV, event.target.value);
+    const { total, next, operation } = calc;
+    let res = '';
+
+    if (total) {
+      res += total;
+    }
+
+    if (operation) {
+      res += operation;
+    }
+
+    if (next) {
+      res += next;
+    }
+
+    if (res.length === 0) res = '0';
+
+    setItem({ calcV: calc, res });
+  };
+
+  const { res } = item;
+
+  return (
+    <div className="App">
+      <Calculator handler={handler} total={res} />
+    </div>
+  );
+};
 
 export default App;
