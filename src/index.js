bcf9ed7e041a1070d1c14d2044ee6208cbb08669
Ibnballo1 <belloabdullateef035@gmail.com>
2022-08-10 00:35:56 +0100

    Add Router to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import
 './index.css';
@@ -9,7 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
