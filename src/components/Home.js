272b9c61f766e077620caec9fe57529d3035f71b
Ibnballo1 <belloabdullateef035@gmail.com>
2022-08-10 08:15:22 +0100

    Update Home js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,72 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable linebreak-style */
 import React from 'react';
+import Header from './Header';
 
 class Home extends React.Component {
   render() {
     return (
       <>
         <div>
-          <h1>Home Page</h1>
+          <Header />
+          <h2>Welcome to our page!</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium omnis officia blanditiis laborum necessitatibus deserunt iusto,
+            sapiente ipsa quibusdam expedita!
+            Optio dolores sapiente esse rem doloribus voluptatibus aliquid repellat iure,
+            reprehenderit totam, earum culpa.
+            Nulla necessitatibus incidunt sunt culpa provident quo neque quae officiis.
+            Voluptates libero cum totam dolore facilis laborum consequatur explicabo minus
+            tempore exercitationem commodi sunt,
+            culpa officiis illo quaerat soluta suscipit alias quas error nam, ad at esse?
+            Consequatur voluptatem atque,
+            architecto suscipit dolor soluta earum.
+            Repellat nihil sapiente nobis hic ipsam,
+            suscipit quos repellendus dignissimos cupiditate?
+            Culpa autem repellendus voluptatibus repellat nemo eos qui reprehenderit fugit nulla,
+            praesentium ut consequatur dicta delectus molestias debitis commodi
+            possimus temporibus ea libero.
+            Placeat consectetur mollitia iste eligendi,
+            illo sequi quod ipsam minima debitis magni provident voluptates aspernatur
+            quo inventore doloribus omnis accusantium.
+            Itaque sapiente rem enim harum eum recusandae,
+            perferendis deleniti quam ex,
+            tenetur maiores facilis.
+            Mollitia blanditiis tempore illo. Voluptatibus.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Molestias dolor error ut eius, saepe illum,
+            quibusdam sunt accusantium officia sint corporis?
+            Sapiente asperiores explicabo blanditiis in,
+            beatae pariatur iusto architecto?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eum amet, dolor ex cupiditate nemo,
+            tempore sint fugiat laudantium, eaque corporis magnam perspiciatis provident asperiores
+            facere iste reprehenderit ab voluptates ipsam non laborum et.
+            Numquam sit a temporibus,
+            consectetur quisquam fugit consequatur amet dolores accusantium assumenda,
+            tenetur quis maiores voluptatibus.
+            Repudiandae est sequi totam,
+            voluptas quidem rem sed necessitatibus.
+            Sed quas quae quibusdam labore perferendis
+            minima temporibus id harum totam nemo commodi sint facilis,
+            doloremque consequuntur nihil.
+            Minima necessitatibus natus sint incidunt quidem doloremque laudantium minus!
+            Quae earum doloribus suscipit aliquam nostrum?
+            Eos officia quaerat expedita,
+            aliquid ducimus voluptatum velit.
+            Vitae quis in laboriosam quisquam voluptas quod perspiciatis
+            quidem nobis similique nisi libero,
+            autem error maiores totam?
+            Totam suscipit doloribus eos nulla necessitatibus illum odio autem possimus,
+            tempore consequatur incidunt dignissimos ratione assumenda at soluta maiores
+            similique quaerat architecto voluptates,
+            recusandae corporis?
+            Explicabo omnis eaque ea vero natus facilis architecto facere commodi nisi?
+            Dolorem aut officiis facere voluptas odio optio ut?
+          </p>
         </div>
       </>
     );
