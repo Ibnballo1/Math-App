6e67aec7e2ab08657a3a38034e9a9466d493d0bc
Ibnballo1 <belloabdullateef035@gmail.com>
2022-08-10 07:42:12 +0100

    Add Quote page

---
+++ src/components/Quote.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+class Quote extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          <h3>Math Quotes</h3>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Quote;
