593c6c8ec0040e461b72bff407dbf93f55177855
Ibnballo1 <belloabdullateef035@gmail.com>
2022-08-10 07:35:57 +0100

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Home Page</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Home;
